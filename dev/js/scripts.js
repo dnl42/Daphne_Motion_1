2326d6492c9729e9476f486d22c226fd0743a40d
dnl42 <dnl42@drexel.edu>
2022-10-19 23:37:52 -0400

    trying things

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,31 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
 import $ from "jquery";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+function scrollAnimation(){
+    var tl = gsap.timeline();
+    tl.to("#hero", {
+        duration: 1,
+        x: 0,
+        scrollTrigger: {
+            trigger: "#hero",
+            // toggleActions: "restart pause reverse pause",
+            markers:true,
+            start: "50px 50%",
+            end: "60% 10%",
+            scrub: true,
+            pin: true
+        }
+    });
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(scrollAnimation());
+
 
 let $nav = $('nav[data-nav]');
 let isVisible = false;
