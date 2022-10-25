12f15785493bd935ae4e5611268ddf310629d9e3
dnl42 <dnl42@drexel.edu>
2022-10-24 23:43:40 -0400

    scroll trigger things

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,26 +5,30 @@ import $ from "jquery";
 gsap.registerPlugin(ScrollTrigger);
 
 
-function scrollAnimation(){
-    var tl = gsap.timeline();
-    tl.to("#hero", {
-        duration: 1,
-        x: 0,
-        scrollTrigger: {
-            trigger: "#hero",
-            // toggleActions: "restart pause reverse pause",
-            markers:true,
-            start: "50px 50%",
-            end: "60% 10%",
-            scrub: true,
-            pin: true
-        }
+// function scrollAnimation(){
+    // var tl = gsap.timeline();
+    // tl.to("#history", {
+        ScrollTrigger.create({
+            trigger: "#history-first",
+            start: "top top",
+            pin:true,
+            pinSpacing:false,
+            markers: true
     });
-    return tl;
-}
 
-var mainTL = gsap.timeline();
-mainTL.add(scrollAnimation());
+    ScrollTrigger.create({
+        trigger: "#gallery-1",
+        start: "top top",
+        scale: 2,
+        pin:true,
+        pinSpacing:false,
+        markers: true
+});
+    // return tl;
+// }
+
+// var mainTL = gsap.timeline();
+// mainTL.add(scrollAnimation());
 
 
 let $nav = $('nav[data-nav]');
