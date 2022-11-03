f8989e60fe28f51e202f6d3a00c0b07489136053
dnl42 <dnl42@drexel.edu>
2022-11-02 22:37:48 -0400

    ui svg progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,34 +7,34 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 function simpleMotion(){
 
       // make a var that represents the line in the SVG
-      var line = document.querySelector("#center-line");
-      // make a var that uses the svg line var from above and sets the BBox property
-      line = line.getBBox();
-      // print out the width of the line. This should make your Figma file
-      // console.log(line.width)
+    //   var line = document.querySelector("#center-line");
+    //   // make a var that uses the svg line var from above and sets the BBox property
+    //   line = line.getBBox();
+    //   // print out the width of the line. This should make your Figma file
+    //   // console.log(line.width)
   
-      var leftBall = document.querySelector("#left-ball");
-      leftBall = leftBall.getBBox();
+    //   var leftBall = document.querySelector("#left-ball");
+    //   leftBall = leftBall.getBBox();
   
-      var center = document.querySelector("#center");
-      center = center.getBBox();
-      // console.log(center.height)
+    //   var center = document.querySelector("#center");
+    //   center = center.getBBox();
+    //   // console.log(center.height)
 
-      gsap.set("#left-ball",{x:line.width / 2 + leftBall.width / 2, transformOrigin:"center" })
-      gsap.set("#right-ball",{x:-line.width/2 - leftBall.width / 2, transformOrigin:"center" })
+    //   gsap.set("#left-ball",{x:line.width / 2 + leftBall.width / 2, transformOrigin:"center" })
+    //   gsap.set("#right-ball",{x:-line.width/2 - leftBall.width / 2, transformOrigin:"center" })
 
-    var tl = gsap.timeline();
-    tl.from("#right-ball",{scale:0, duration:0.25, drawSVG: 0})
-      .from("#left-ball",{scale:0, duration:0.25},"-=50%")
-      .to("#right-ball",{x:0, duration:0.25},"rollOut")
-      .to("#left-ball",{x:0, duration:0.25},"rollOut")
-      .fromTo("#center-line",{drawSVG:"50% 50%"},{duration:0.25, drawSVG: "0% 100%"},"rollOut")
-      .from("#center",{duration: 0.25, scale:0, transformOrigin:"center"},"-=50%")
-      .to("#center",{duration: 0.25, y: -center.height / 2 - 50},"-=50%")
-      .to("#center",{duration: 0.15, y: 0})
-      .fromTo("#center-outline",{drawSVG:"100% 50%"},{duration:0.15,drawSVG:"125% 25%"},"-=50%")
+    // var tl = gsap.timeline();
+    // tl.from("#right-ball",{scale:0, duration:0.25, drawSVG: 0})
+    //   .from("#left-ball",{scale:0, duration:0.25},"-=50%")
+    //   .to("#right-ball",{x:0, duration:0.25},"rollOut")
+    //   .to("#left-ball",{x:0, duration:0.25},"rollOut")
+    //   .fromTo("#center-line",{drawSVG:"50% 50%"},{duration:0.25, drawSVG: "0% 100%"},"rollOut")
+    //   .from("#center",{duration: 0.25, scale:0, transformOrigin:"center"},"-=50%")
+    //   .to("#center",{duration: 0.25, y: -center.height / 2 - 50},"-=50%")
+    //   .to("#center",{duration: 0.15, y: 0})
+    //   .fromTo("#center-outline",{drawSVG:"100% 50%"},{duration:0.15,drawSVG:"125% 25%"},"-=50%")
 
-    return tl;
+    // return tl;
 
 }
 
@@ -45,28 +45,37 @@ function patternMotion(){
     tl.from(".odd",{duration:2, drawSVG:0, stagger:0.25, rotate:180, transformOrigin:"center"},"spin")
     .fromTo(".even",{drawSVG:"0% 0%"},{duration:2, drawSVG:"0% -100%", stagger:0.25, rotate:180, transformOrigin:"center"},"spin");
     return tl;
-
 }
 
 function UIMotion(){
 
 
     // make a var that represents the line in the SVG
-    var bottomLine = document.querySelector("#bottom-line");
-    // make a var that uses the svg line var from above and sets the BBox property
-    bottomLine = bottomLine.getBBox();
+    // var bottomLine = document.querySelector("#bottom-line");
+    // // make a var that uses the svg line var from above and sets the BBox property
+    // bottomLine = bottomLine.getBBox();
 
-    console.log(bottomLine.width);
+    // console.log(bottomLine.width);
+
+    var first = document.querySelector("#Rectangle-1");
+      first = first.getBBox();
+      console.log(first.height);
 
     var tl = gsap.timeline();
-    tl.to("#pencil",{duration:0.5,x:bottomLine.width},"draw")
-    .from("#bottom-line",{duration:0.5,drawSVG:0},"draw")
-    .to("#pencil",{duration:0.25, alpha:0},"drawOutline")
-    .fromTo("#outline",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"},"drawOutline")
-    .from("#box-bg",{duration:0.5, scaleX:0})
-    .from(".letter",{duration:0.25, alpha:0, y:20, stagger: 0.12},"-=50%")
-    .from("#glass",{duration:0.25, drawSVG:0},"-=.5")
-    .from("#handle",{duration:0.25, drawSVG:0},"-=.25")
+    tl.from ("#Rectangle-1",{duration: 0.25, scale:0, transformOrigin:"first"},"-=50%"),
+    tl.to ("#Rectangle-1",{duration: 0.25, x: -first.height / 2},"-=50%"),
+
+    tl.from ("#Rectangle-2",{duration: 0.25, scale:0, transformOrigin:"first"},"-=50%"),
+    tl.to ("#Rectangle-2",{duration: 0.25, x: -first.height / 2},"-=50%"),
+
+    tl.from ("#Rectangle-3",{duration: 0.25, scale:0, transformOrigin:"first"},"-=50%"),
+    tl.to ("#Rectangle-3",{duration: 0.25, x: -first.height / 2},"-=50%"),
+
+    tl.from ("#Rectangle-4",{duration: 0.25, scale:0, transformOrigin:"first"},"-=50%"),
+    tl.to ("#Rectangle-4",{duration: 0.25, x: -first.height / 2},"-=50%"),
+
+    tl.from ("#Rectangle-5",{duration: 0.25, scale:0, transformOrigin:"first"},"-=50%"),
+    tl.to ("#Rectangle-5",{duration: 0.25, x: -first.height / 2},"-=50%")
     return tl;
 
 }
