b246bd670778367e85cc12474863ae9f0fddb005
dnl42 <dnl42@drexel.edu>
2022-12-01 10:03:05 -0500

    moving

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,10 +11,12 @@ MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
 function Shapes(){
 
     var tl = gsap.timeline();
-    tl.to("#shape1",{duration: .5, rotate: 90, transformOrigin: "center center", x:150})
-    tl.to("#shape2",{duration: .5, rotate: -90, transformOrigin: "center center", y:150})
-    tl.to("#shape3",{duration: .5, rotate: 180, transformOrigin: "center center", y:250})
-    tl.to("#shape4",{duration: .5, rotate: 90, transformOrigin: "center center", y:200})
+    tl.from("#shape1",{duration: .25, stagger: 0.25, rotate: 90, transformOrigin: "center center", x:150})
+    tl.from("#shape2",{duration: .25, stagger: 0.25, rotate: -90, transformOrigin: "center center", y:150})
+    tl.from("#shape3",{duration: .25, stagger: 0.25, rotate: 180, transformOrigin: "center center", y:250})
+    tl.from("#shape4",{duration: .25, rotate: 90, transformOrigin: "center center", y:200})
+    tl.from("#shape5",{duration: .25, rotate: 90, transformOrigin: "center center", x:200})
+    tl.from("#shape7",{duration: .25, rotate: -90, transformOrigin: "center center", x:300})
 
     return tl;
 }
