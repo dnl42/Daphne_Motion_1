06188335d7257d81a207aedbeb1ef96feb39d5a0
dnl42 <dnl42@drexel.edu>
2022-12-07 17:53:30 -0500

    things

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -38,9 +38,19 @@ function Plants(){
     tl.from("#plant1", 1,{alpha: 0, morphSVG: "#plant2"})
     tl.from("#plant2", 1,{alpha: 0, morphSVG: "#flower"})
 
+    return tl;
+}
+
+
+function Name(){
+
+    var tl = gsap.timeline();
+    tl.to("#WORD", {drawSVG:"100%", duration:2, stagger:0.2})
+
 
     return tl;
 }
+
 
 
 
@@ -50,13 +60,5 @@ GSDevTools.create();
 var mainTL = gsap.timeline();
 mainTL.add(Shapes())
 mainTL.add(Plants())
+mainTL.add(Name())
 
-
-
-// var tl = gsap.timeline();
-// tl.from("#shape1",{duration: .25, stagger: 0.25, rotate: 90, transformOrigin: "center center", x:150})
-// tl.from("#shape2",{duration: .25, stagger: 0.25, rotate: -90, transformOrigin: "center center", y:150})
-// tl.from("#shape3",{duration: .25, stagger: 0.25, rotate: 180, transformOrigin: "center center", y:250})
-// tl.from("#shape4",{duration: .25, rotate: 90, transformOrigin: "center center", y:200})
-// tl.from("#shape5",{duration: .25, rotate: 90, transformOrigin: "center center", x:200})
-// tl.from("#shape7",{duration: .25, rotate: -90, transformOrigin: "center center", x:300})
